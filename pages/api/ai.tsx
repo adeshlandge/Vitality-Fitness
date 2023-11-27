26787fa7be6eff5e1aa23300838eb74def4cc6a3
Adesh Landge <37284267+adeshlandge@users.noreply.github.com>
2023-11-26 23:38:46 -0800

    Update ai.tsx

--- pages/api/ai.tsx
+++ pages/api/ai.tsx
@@ -8,16 +8,21 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: req.body.prompt, // Text prompt to generate continuation from, passed in as a request body parameter
-    temperature: 1,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 2500, // Maximum number of tokens to generate in the output text
-  });
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: req.body.prompt, // Text prompt to generate continuation from, passed in as a request body parameter
+      temperature: 1,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 2500, // Maximum number of tokens to generate in the output text
+    });
 
-  // Send a response with a status code of 200 and the generated text as the response body
-  res.status(200).json({ result: completion.data });
+    // Send a response with a status code of 200 and the generated text as the response body
+    res.status(200).json({ result: completion.data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
