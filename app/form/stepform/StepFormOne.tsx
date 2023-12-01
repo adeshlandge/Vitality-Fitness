dc4db86c81ef2e313b080906b6a3da2beff4d308
Adesh Landge <37284267+adeshlandge@users.noreply.github.com>
2023-11-30 19:54:07 -0800

    Update StepFormOne.tsx

--- app/form/stepform/StepFormOne.tsx
+++ app/form/stepform/StepFormOne.tsx
@@ -10,42 +10,42 @@ const StepForm1 = () => {
     <div className=" p-20 max-sm:mx-auto max-sm:p-8">
       <div className="mb-10 font-product max-sm:flex max-sm:flex-col max-sm:justify-center">
         <h1 className="text-3xl font-semibold leading-[2.4rem] w-[30ch] max-sm:w-[16ch] max-sm:text-2xl">
-          Fill out our progress form to receive a customized diet plan.
+          Enter your preferences.
         </h1>
-        <p>Tailored to your unique needs and preferences.</p>
+{/*         <p>Tailored to your unique needs and preferences.</p> */}
       </div>
       <div className="grid grid-cols-2 gap-8 max-sm:grid-cols-1">
         <TextField
           required
           value={state.weight}
           onChange={(e) => state.setWeight(e.target.value)}
-          label="weight (in lbs)"
+          label="Enter your Weight (in lbs)"
           variant="outlined"
         />
         <TextField
           required
           value={state.height}
           onChange={(e) => state.setHeight(e.target.value)}
-          label="height"
+          label="Enter your Height"
           variant="outlined"
         />
         <TextField
           required
           value={state.age}
           onChange={(e) => state.setAge(e.target.value)}
-          label="age"
+          label="Enter your Age"
           variant="outlined"
         />
         <TextField
           value={state.diseases}
           onChange={(e) => state.setDisease(e.target.value)}
-          label="diseases"
+          label="Diseases (if any)"
           variant="outlined"
         />
         <TextField
           value={state.allergies}
           onChange={(e) => state.setAllergies(e.target.value)}
-          label="allergies"
+          label="Allergies (if any)"
           variant="outlined"
         />
       </div>
